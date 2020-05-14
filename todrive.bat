0726c27312963fb69817235e5558c1eb49989fff
weive735 <weive735@gmail.com>
2020-05-14 20:34:17 +0900

    update

--- todrive.bat
+++ todrive.bat
@@ -3,7 +3,7 @@
 set ONEDRIVE=%USERPROFILE%\OneDrive\downloads\myenvmig
 
 xcopy /D /Y /S pack %ONEDRIVE%\pack\ && ^
-xcopy /D /Y tool %ONEDRIVE%\tool\ && ^
+xcopy /D /Y /S tool %ONEDRIVE%\tool\ && ^
 xcopy /D /Y pack.bat %ONEDRIVE% && ^
 xcopy /D /Y pack.ps1 %ONEDRIVE% && ^
 xcopy /D /Y unpack.bat %ONEDRIVE% && ^
