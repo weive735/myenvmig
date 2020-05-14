9a0891d4e736d0bba1f2b26fd1423fe88bd772c8
weive735 <weive735@gmail.com>
2020-05-14 17:54:42 +0900

    impl pack.ps1

---
+++ pack.ps1
@@ -0,0 +1,17 @@
+$env:Path += Convert-Path .\tool\7zip
+
+if (!(Test-Path .\pack)) { mkdir .\pack }
+
+$packDir = Convert-Path .\pack
+$scoopDir = Convert-Path ~\scoop
+
+$scoopPack = $(Join-Path $packDir scoop.7z)
+
+
+# PACKING SCOOP
+if (Test-Path $scoopPack) {
+    $res = Remove-Item -Confirm $scoopPack
+}
+if (!(Test-Path $scoopPack)) {
+    7z.exe a -bsp1 -bt $scoopPack $scoopDir
+}
